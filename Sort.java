e1829e7fb549b78200b03a80fed2a53a16431508
Harshit <hcool2489@gmail.com>
2020-01-24 12:00:54 +0530

    Insertion Sort Added

--- Sort.java
+++ Sort.java
@@ -33,8 +33,18 @@ public class Sort{
 	
 	//Insertion Sort
 	private static int[] insertionSort(int[] arr){
-		
-	}
+        int key, i, j;
+        for(j=1;j<arr.length;j++){
+            key = arr[j];
+            i=j-1;
+            while(i>-1 && arr[i]>key){
+                arr[i+1] = arr[i];
+                i--;
+            }
+            arr[i+1] = key;
+        }
+        return arr;
+    }
 	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
